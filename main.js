321e897535497e8f08ea701ff24c8e52140e62a4
Rémi Épaulais <epaulais.remi@gmail.com>
2023-11-14 17:20:04 +0100

    Added Audio

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 document.getElementsByClassName("party")[0].addEventListener("click", () => {
   let container = document.getElementsByTagName("main")[0];
   let canvas = document.createElement("canvas");
+  const audioHorn = document.getElementById("audio");
+
+  const randomPitch = Math.random() * (1.15 - 0.85) + 0.85;
+  audioHorn.playbackRate = randomPitch;
+  audioHorn.preservesPitch = false;
+  audioHorn.volume = 0.5;
+  audioHorn.play();
 
   canvas.width = document.documentElement.clientWidth;
   canvas.height = document.documentElement.clientHeight;
@@ -8,11 +15,13 @@ document.getElementsByClassName("party")[0].addEventListener("click", () => {
 
   let confetti_button = confetti.create(canvas);
   confetti_button({
-    particleCount: 10,
+    particleCount: 50,
     spread: 360,
     startVelocity: 20,
-    scalar: 1,
+    scalar: 0.8,
     ticks: 30,
-    gravity: 0,
+    gravity: .3,
   })
+
+
 })
